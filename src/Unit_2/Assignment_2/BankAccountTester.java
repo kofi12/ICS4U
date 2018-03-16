2c031c85771c3ecef1c5e7488ea1923ffe702729
aaron haizel <aaronhaizel@gmail.com>
2018-03-16 19:47:28 -0400

    "Implemented BankAccountTester.java for you to test your classes"

--- src/Unit_2/Assignment_2/BankAccountTester.java
+++ src/Unit_2/Assignment_2/BankAccountTester.java
@@ -8,9 +8,35 @@ public class BankAccountTester
     {
         Scanner in = new Scanner(System.in);
 
-        CheckingAccount check = new CheckingAccount();
-        SavingsAccount save = new SavingsAccount();
+        BankAccount someAccount;
 
+
+        double depo;
+        double wd;
+        int selection = 0;
+
+        System.out.println("1: CheckingAccount\n2: SavingsAccount");
+
+        if((selection = in.nextInt()) == 1)
+        {
+            CheckingAccount checkingAccount = new CheckingAccount("1234", "Johnny", 0);
+            System.out.println("deposit an amount: ");
+            checkingAccount.deposit(depo = in.nextDouble());
+            System.out.println("withdraw an amount: ");
+            checkingAccount.withdraw(wd = in.nextDouble());
+            someAccount = checkingAccount;
+        }
+        else
+        {
+            SavingsAccount savingsAccount = new SavingsAccount("5678", "Jane", 0);
+            System.out.println("deposit an amount: ");
+            savingsAccount.deposit(depo = in.nextDouble());
+            System.out.println("withdraw an amount: ");
+            savingsAccount.withdraw(wd = in.nextDouble());
+            someAccount = savingsAccount;
+        }
+
+        System.out.println("Here is your banking information...\n" + someAccount);
 
     }
 }
